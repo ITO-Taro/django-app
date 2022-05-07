6dc3dda0d94865becf99473f11216173706d4d25
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-05-06 21:09:14 -0400

    installed WhiteNoise and added it to middle_ware and staticfiles_storage to settings.py

--- health_care_portal/settings.py
+++ health_care_portal/settings.py
@@ -27,7 +27,7 @@ WEATHER_TOKEN = "15c2357e8154d5f7e3e2e0f7c90d42d8"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-db-api-app.herokuapp.com' ]
+ALLOWED_HOSTS = ['django-db-api-app.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'health_care_portal.urls'
@@ -124,12 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
